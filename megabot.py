1635c97324749eb18c085ed0b2f368199a1088ac
Mathias Vandaele <contactvandaele@gmail.com>
2021-04-21 10:19:32 +0200

    update requirement

--- megabot.py
+++ megabot.py
@@ -7,18 +7,20 @@ import sys
 from utils.data_management import DataManagement
 
 
-def main(args):
-    if args.mode == "train":
+def main(parsed_args):
+    if parsed_args.mode == "train":
         print("training mode starting")
-    elif args.mode == "run":
+    elif parsed_args.mode == "run":
         print("running mode starting")
-    elif args.mode == "backtest":
+    elif parsed_args.mode == "backtest":
         print("back testing mode starting")
-    elif args.mode == "freshdata":
-        if args.pair is None:
-            sys.exit("For this mode; you need to set --pair to select the pair you want to fetch the data")
-        if args.date is None:
-            sys.exit("For this mode; you need to set --pair to select the date you want to fetch the data from ")
+    elif parsed_args.mode == "freshdata":
+        if parsed_args.pair is None:
+            sys.exit("For this mode; you need to set --pair to select the pair you want to fetch "
+                     "the data")
+        if parsed_args.date is None:
+            sys.exit("For this mode; you need to set --pair to select the date you want to fetch "
+                     "the data from ")
         print("retrieving fresh data ...")
         DataManagement.retrieve_and_save_data(args.pair, args.date)
 
@@ -30,5 +32,5 @@ if __name__ == '__main__':
     parser.add_argument("--pair", help="The pair you want to run the bot on or retrieve datas")
     parser.add_argument("--date", help="If retrieving datas, set the date you want to get the datas from (ex : '1 "
                                        "Jan, 2020'")
-    args = parser.parse_args()
-    main(args)
+    parsed_args = parser.parse_args()
+    main(parsed_args)
